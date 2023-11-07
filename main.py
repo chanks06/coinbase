a132c7dce428a90cd651117ab202839c58d539b4
Charles Hanks <chanks@willamette.edu>
2023-11-07 09:29:42 -0800

    full main.py script for ETL

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ from coinbase.wallet.client import Client # for interacting with coinbase API
 from dotenv import load_dotenv #for using environmental variables 
 import psycopg2 # for creating python-postgres connection 
 from datetime import datetime 
-from email.utils import parsedate_to_datetime
-import pytz
 
 def main(): 
     # Loading environment variables
@@ -37,7 +35,7 @@ def main():
     #initializing cursor
     cursor = connection.cursor() 
 
-# EXTRACT + TRANSFORM 
+    # EXTRACT + TRANSFORM 
 
     #establishing connection to coinbase API with credentials 
     client = Client(api_key, api_secret)
@@ -53,79 +51,72 @@ def main():
         #creating dictionary of 3 coins by calling spot_price function
         prices = {i: spot_price(client, i) for i in coins}
 
-        #adding timestamp 
-        prices["datetime"] = datetime.now()
-        
         #throw an error if any coin data is not available
         if any(not price for price in prices.values()):
             raise ValueError("Incomplete data received from API")
-        
-    except Exception as e:
-        # error log 
-        print(f"An error occurred: {e}")
 
     # LOAD 
 
-        # creating 3 tables for each coin in postgres db
+        #creating schema for organize tables: 
+        cursor.execute("CREATE SCHEMA IF NOT EXISTS crypto;")
+        cursor.execute("GRANT ALL PRIVELEGS ON SCHEMA crypto to postgres;")
+
+        #making sure timestamp will display in my local time
+        cursor.execute("SET TIMEZONE = 'America/Los_Angeles':") 
+
+        connection.commit() 
+
+                # creating 3 tables for each coin in postgres db
         table_schema_btc = ("""
-                    CREATE TABLE IF NOT EXISTS bitcoin
-                    (id serial PRIMARY KEY, 
-                     datetime timestamp, 
-                     base varchar(10), 
-                     amount numeric(10,4),
-                     currency char(3));
-                    """)
-        
+                            CREATE TABLE IF NOT EXISTS crypto.bitcoin
+                            (id serial PRIMARY KEY, 
+                            datetime timestamp, 
+                            base varchar(10), 
+                            amount numeric(10,4),
+                            currency char(3));
+                            """)
+                
         table_schema_eth = ("""
-                    CREATE TABLE IF NOT EXISTS ethereum
-                    (id serial PRIMARY KEY, 
-                     datetime timestamp, 
-                     base varchar(10), 
-                     amount numeric(10,4),
-                     currency char(3));
-                    """)
-        
+                            CREATE TABLE IF NOT EXISTS crypto.ethereum
+                            (id serial PRIMARY KEY, 
+                            datetime timestamp, 
+                            base varchar(10), 
+                            amount numeric(10,4),
+                            currency char(3));
+                            """)
+                
         table_schema_near = ("""
-                    CREATE TABLE IF NOT EXISTS near
-                    (id serial PRIMARY KEY, 
-                     datetime timestamp, 
-                     base varchar(10), 
-                     amount numeric(10,4),
-                     currency char(3));
-                    """)
-        
+                            CREATE TABLE IF NOT EXISTS crypto.near
+                            (id serial PRIMARY KEY, 
+                            datetime timestamp, 
+                            base varchar(10), 
+                            amount numeric(10,4),
+                            currency char(3));
+                            """)
+                
         tables = [table_schema_btc, table_schema_eth, table_schema_near]
 
         #iterating through create table statements 
         for t in tables: 
             cursor.execute(t)
-        
+            
         connection.commit()
 
-        #inserting data into tables: 
-        insert_query = """ INSERT INTO bitcoin (datetime, base, amount, currency)
-                            VALUES (%(datetime)s, %(base)s, %(amount)s, %(currency)s);
-                            """
+            #inserting data into tables: 
+        insert_query_btc = """ INSERT INTO crypto.bitcoin (datetime, base, amount, currency)
+                                VALUES (CURRENT_TIMESTAMP, %(base)s, %(amount)s, %(currency)s);
+                                """
+        insert_query_eth = """ INSERT INTO crypto.ethereum (datetime, base, amount, currency)
+                                VALUES (CURRENT_TIMESTAMP, %(base)s, %(amount)s, %(currency)s);
+                                """
+        insert_query_near = """ INSERT INTO crypto.near (datetime, base, amount, currency)
+                                VALUES (CURRENT_TIMESTAMP, %(base)s, %(amount)s, %(currency)s);
+                                """
+        
 
-
-        cursor.execute(insert_query, price_data["BTC-USD"])
-
-
-        cursor.execute(table_schema)
-
-        #adding moon data dictionary to railway postgresql db
-        insert_query = (
-            """
-            INSERT INTO moon (local_datetime, api_datetime, phase, illumination, age_days, lunar_cycle, phase_name, stage, emoji, zodiac_sign, 
-            moonrise, moonrise_timestamp, moonset, moonset_timestamp, moon_altitude, moon_distance, 
-            moon_azimuth, moon_parallactic_angle) 
-            VALUES (%(local_datetime)s, %(date)s, %(phase)s, %(illumination)s, %(age_days)s, %(lunar_cycle)s, %(phase_name)s,
-            %(stage)s, %(emoji)s, %(zodiac_sign)s, %(moonrise)s, %(moonrise_timestamp)s, %(moonset)s, 
-            %(moonset_timestamp)s, %(moon_altitude)s, %(moon_distance)s, %(moon_azimuth)s, %(moon_parallactic_angle)s);
-            """
-        )
-
-        cursor.execute(insert_query, moon_dict)
+        cursor.execute(insert_query_btc, prices["BTC-USD"])
+        cursor.execute(insert_query_eth, prices["ETH-USD"])
+        cursor.execute(insert_query_near, prices["NEAR-USD"])
 
         connection.commit()
 
@@ -149,8 +140,8 @@ def main():
 
 while True: 
     main() 
-    print("sleeping for 24h)")
-    time.sleep(24*60*60)
+    print("sleeping for 4 hours)")
+    time.sleep(14400)
 
 
 
